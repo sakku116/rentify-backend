bf1b889f792470828b1475a374c2a70adafe674c
zakky <zakky1164@gmail.com>
2023-10-27 11:20:02 +0700

    new request payloads

--- dto/request.go
+++ dto/request.go
@@ -8,3 +8,15 @@ type PostLoginReq struct {
 type PostCheckTokenReq struct {
 	Token string `json:"token"`
 }
+
+type PostSetRoleFromTokenReq struct {
+	Token string `json:"token"`
+	Role  string `json:"role"`
+}
+
+type PostRegisterReq struct {
+	Username        string `json:"username"`
+	Email           string `json:"email"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirm_password"`
+}
