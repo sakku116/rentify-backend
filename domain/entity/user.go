774d898cd6832c6d625905b746926bca04ebcd97
zakky <zakky1164@gmail.com>
2023-10-31 17:55:39 +0700

    nothin

--- domain/entity/user.go
+++ domain/entity/user.go
@@ -2,15 +2,15 @@ package entity
 
 type User struct {
 	ID        string `json:"id" bson:"id"`
+	CreatedAt int64  `json:"created_at" bson:"created_at"`
+	UpdatedAt int64  `json:"updated_at" bson:"updated_at"`
+	CreatedBy string `json:"created_by" bson:"created_by"`
+	UpdatedBy string `json:"updated_by" bson:"updated_by"`
+
 	Username  string `json:"username" bson:"username"`
 	Email     string `json:"email" bson:"email"`
 	Password  string `json:"password" bson:"password"`
 	IsActive  bool   `json:"is_active,omitempty" bson:"is_active"`
 	SessionID string `json:"session_id" bson:"session_id"`
 	Role      string `json:"role" bson:"role"`
-
-	CreatedAt int64  `json:"created_at" bson:"created_at"`
-	UpdatedAt int64  `json:"updated_at" bson:"updated_at"`
-	CreatedBy string `json:"created_by" bson:"created_by"`
-	UpdatedBy string `json:"updated_by" bson:"updated_by"`
 }
--- utils/helper/main.go
+++ utils/helper/main.go
@@ -2,9 +2,7 @@ package helper
 
 import (
 	"fmt"
-	"reflect"
 	"rentify/config"
-	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -19,47 +17,6 @@ func ComparePasswordHash(password string, hash string) bool {
 
 func GenerateUUID() string {
 	return uuid.New().String()
-}
-
-func ParseBsonPatchStruct(obj interface{}) map[string]interface{} {
-	var result = make(map[string]interface{})
-	val := reflect.ValueOf(obj).Elem()
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Type().Field(i)
-		tag := field.Tag.Get("bson_patch")
-
-		if tag != "" {
-			tagParts := strings.Split(tag, ",")
-			tagName := tagParts[0]
-			omitempty := false
-
-			if len(tagParts) > 1 && tagParts[1] == "omitempty" {
-				omitempty = true
-			}
-
-			if omitempty && IsZero(val.Field(i)) {
-				continue
-			}
-
-			value := val.Field(i).Interface()
-			result[tagName] = value
-		}
-	}
-
-	return result
-}
-
-func IsZero(value reflect.Value) bool {
-	switch value.Kind() {
-	case reflect.Interface, reflect.Ptr:
-		return value.IsNil()
-	case reflect.Slice, reflect.Map, reflect.String:
-		return value.Len() == 0
-	default:
-		zeroValue := reflect.Zero(value.Type()).Interface()
-		return reflect.DeepEqual(value.Interface(), zeroValue)
-	}
 }
 
 /*
