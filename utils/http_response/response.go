f6c5042cbc4f89dba0ddd441b6a720d2b36c80d9
zakky <zakky1164@gmail.com>
2023-11-10 16:51:38 +0700

    use rest.BaseJSONResp schema in response writer

--- utils/http_response/response.go
+++ utils/http_response/response.go
@@ -1,6 +1,7 @@
 package http_response
 
 import (
+	"rentify/domain/rest"
 	error_utils "rentify/utils/error"
 
 	"github.com/gin-gonic/gin"
@@ -21,36 +22,36 @@ func NewResponseWriter() IResponseWriter {
 func (r *ResponseWriter) HTTPCustomErr(ctx *gin.Context, err error) {
 	customErr, ok := err.(*error_utils.CustomErr)
 	if ok {
-		ctx.JSON(customErr.Code, gin.H{
-			"error":   true,
-			"message": customErr.Error(),
-			"detail":  "",
-			"data":    nil,
+		ctx.JSON(customErr.Code, rest.BaseJSONResp{
+			Error:   true,
+			Message: customErr.Error(),
+			Detail:  "",
+			Data:    nil,
 		})
 		return
 	}
-	ctx.JSON(500, gin.H{
-		"error":   true,
-		"message": "internal server error",
-		"detail":  err.Error(),
-		"data":    nil,
+	ctx.JSON(500, rest.BaseJSONResp{
+		Error:   true,
+		Message: "internal server error",
+		Detail:  err.Error(),
+		Data:    nil,
 	})
 }
 
 func (r *ResponseWriter) HTTPJsonErr(ctx *gin.Context, code int, message string, detail string, data interface{}) {
-	ctx.JSON(code, gin.H{
-		"error":   true,
-		"message": message,
-		"detail":  detail,
-		"data":    data,
+	ctx.JSON(code, rest.BaseJSONResp{
+		Error:   true,
+		Message: message,
+		Detail:  detail,
+		Data:    data,
 	})
 }
 
 func (r *ResponseWriter) HTTPJson(ctx *gin.Context, data interface{}) {
-	ctx.JSON(200, gin.H{
-		"error":   false,
-		"message": "OK",
-		"detail":  "",
-		"data":    data,
+	ctx.JSON(200, rest.BaseJSONResp{
+		Error:   false,
+		Message: "OK",
+		Detail:  "",
+		Data:    data,
 	})
 }
